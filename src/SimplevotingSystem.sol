667e364be9dfa280dc8b18b481e76ab0202bb743
JulienPajot <julien.pajot@dupdevinci-edu.fr>
2026-01-15 12:42:55 +0100

    feat: add role Admin

---
+++ src/SimplevotingSystem.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract SimpleVotingSystem  is Ownable{
+    
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+    uint[] private candidateIds;
+
+    constructor() Ownable(msg.sender) {}
+
+    function addCandidate(string memory _name) external onlyRole(ADMIN_ROLE) {
+
+        require(bytes(_name).length > 0, "Candidate name cannot be empty");
+        uint candidateId = candidateIds.length + 1;
+        candidates[candidateId] = Candidate(candidateId, _name, 0);
+        candidateIds.push(candidateId);
+    }
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender], "You have already voted");
+        require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount += 1;
+    }
+
+    function getTotalVotes(uint _candidateId) public view returns (uint) {
+        require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
+        return candidates[_candidateId].voteCount;
+    }
+
+    function getCandidatesCount() public view returns (uint) {
+        return candidateIds.length;
+    }
+
+    // Optional: Function to get candidate details by ID
+    function getCandidate(uint _candidateId) public view returns (Candidate memory) {
+        require(_candidateId > 0 && _candidateId <= candidateIds.length, "Invalid candidate ID");
+        return candidates[_candidateId];
+    }
+}
